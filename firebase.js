c58a2f9b25bd804bdc1978a5a5433a3bcda69eaf
Dhanvinesh K <75219613+Reaper-Dhan@users.noreply.github.com>
2021-09-13 15:41:44 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBCn5beig99gH-__f7d5nthvwC9YpnnQe0",
+    authDomain: "login-6cd3b.firebaseapp.com",
+    projectId: "login-6cd3b",
+    storageBucket: "login-6cd3b.appspot.com",
+    messagingSenderId: "27487814893",
+    appId: "1:27487814893:web:febd99d6649f206a1cb8e4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
